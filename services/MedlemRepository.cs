ed997f8713b67cb41c90ecccfff72dfc68c57c22
Søren Vinther Hansen <soh003@edu.zealand.dk>
2023-12-08 11:18:04 +0100

    opret medlemrepo

---
+++ services/MedlemRepository.cs
@@ -0,0 +1,20 @@
+﻿using joga.dk.model;
+
+namespace joga.dk.services
+{
+
+    public class MedlemRepository
+    {
+        //Instansfelt og properties(Get/Set)
+        public List<Medlem> MedlemList { get; set; }
+
+        //Konstruktør
+        public MedlemRepository()
+        {
+            MedlemList = new List<Medlem>();
+        }
+
+    }
+
+    
+}
